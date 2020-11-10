cbad421ef872b14f6841c76b189378d64dfc2c62
vaughnGOAT <sevaughnorr@gmail.com>
2020-11-10 14:47:14 -0500

    - renamed Diamonds10.png DiamondsTen.png - moved game bar to right side - fixed listener stacking - menus now render immediately

--- MySolitaire-master/Solitaire.java
+++ MySolitaire-master/Solitaire.java
@@ -25,14 +25,25 @@ public class Solitaire {
     private static CardStack[] playCardStack; // Tableau stacks
     private static CardStack deck; // populated with standard 52 card deck
     // other components
+    private static int gameBarXOff = TABLE_WIDTH - 120;
+    private int gameBarCompXOff = 0;
+    private static int gameBarYOff = TABLE_HEIGHT - 490;
+    private static int gameBarCompYOff = 40;
+    private static int numGameBarComps = 0;
     private static JEditorPane gameTitle = new JEditorPane("text/html", "");
     private static JButton showRulesButton = new JButton("Show Rules");
     private static JButton newGameButton = new JButton("New Game");
     private static JButton toggleTimerButton = new JButton("Pause Timer");
-    private static JButton mainMenuButton= new JButton("Main Menu");
     private static JTextField scoreBox = new JTextField();// displays the score
     private static JTextField timeBox = new JTextField();// displays the time
     private static JTextField statusBox = new JTextField();// status messages
+
+    public static JButton getMainMenuButton() {
+        return mainMenuButton;
+    }
+
+    private static JButton mainMenuButton= new JButton("Main Menu");
+    private static JButton optionsMenuButton = new JButton("Options Menu");
     // TIMER UTILITIES
     private static Timer timer = new Timer();
     private static ScoreClock scoreClock = new ScoreClock();
@@ -44,6 +55,12 @@ public class Solitaire {
 
     public static int getTableHeight() {
         return TABLE_HEIGHT;
+    }
+
+    public static int incGameBarCompYOff()
+    {
+        numGameBarComps += 1;
+        return gameBarCompYOff * numGameBarComps;
     }
 
     public static int getTableWidth() {
@@ -158,6 +175,16 @@ public class Solitaire {
         return time;
     }
 
+    public static void addListenerIfNotPresent(JButton button, ActionListener listener)
+    {
+        ActionListener[] currentListeners = button.getActionListeners();
+        for (ActionListener l: currentListeners)
+        {
+            button.removeActionListener(l);
+        }
+        button.addActionListener(listener);
+    }
+
     // moves a card to abs location within a component
     protected static Card moveCard(Card c, int x, int y) {
         c.setBounds(new Rectangle(new Point(x, y), new Dimension(Card.CARD_WIDTH + 10, Card.CARD_HEIGHT + 10)));
@@ -195,9 +222,10 @@ public class Solitaire {
     }
 
     private static void playNewGame() {
+        numGameBarComps = 0;
+        table.removeAll();
         deck = new CardStack(true); // deal 52 cards
         deck.shuffle();
-        table.removeAll();
         // reset stacks if user starts a new game in the middle of one
         resetStacks();
         // initialize & place final (foundation) decks/stacks
@@ -216,12 +244,11 @@ public class Solitaire {
 
         startTimer();
 
-        toggleTimerButton.setBounds(480, TABLE_HEIGHT - 70, 120, 30);
-        toggleTimerButton.addActionListener(new ToggleTimerListener());
 
-        statusBox.setBounds(605, TABLE_HEIGHT - 70, 180, 30);
+
+        /*statusBox.setBounds(605, TABLE_HEIGHT - 70, 180, 30);
         statusBox.setEditable(false);
-        statusBox.setOpaque(false);
+        statusBox.setOpaque(false);*/
 
         table.add(statusBox);
         table.add(toggleTimerButton);
@@ -235,44 +262,66 @@ public class Solitaire {
     }
 
     private static void initializeButtons() {
-        newGameButton.addActionListener(new NewGameListener());
-        newGameButton.setBounds(0, TABLE_HEIGHT - 70, 120, 30);
 
-        showRulesButton.addActionListener(new ShowRulesListener());
-        showRulesButton.setBounds(120, TABLE_HEIGHT - 70, 120, 30);
+        addListenerIfNotPresent(newGameButton, new NewGameListener());
+        newGameButton.setBounds(gameBarXOff, gameBarYOff + incGameBarCompYOff(), 120, 30);
 
-        mainMenuButton.addActionListener(new MainMenuListener());
-        mainMenuButton.setBounds(600,TABLE_HEIGHT-70,120,30);
+        addListenerIfNotPresent(showRulesButton, new ShowRulesListener());
+        showRulesButton.setBounds(gameBarXOff, gameBarYOff + incGameBarCompYOff(), 120, 30);
+
+        addListenerIfNotPresent(mainMenuButton, new MainMenuListener());
+        mainMenuButton.setBounds(TABLE_WIDTH - 120, gameBarYOff + incGameBarCompYOff(),120,30);
 
         gameTitle.setText("<b>Demon Fan Solitaire</b> <br> CPSC4900 <br> Group 2");
         gameTitle.setEditable(false);
         gameTitle.setOpaque(false);
         gameTitle.setBounds(245, 20, 100, 100);
 
-        scoreBox.setBounds(240, TABLE_HEIGHT - 70, 120, 30);
+        toggleTimerButton.setBounds(TABLE_WIDTH - 120, gameBarYOff + incGameBarCompYOff(), 120, 30);
+        addListenerIfNotPresent(toggleTimerButton, new ToggleTimerListener());
+
+        scoreBox.setBounds(TABLE_WIDTH - 120, gameBarYOff + incGameBarCompYOff(), 120, 30);
         scoreBox.setText("Score: 0");
         scoreBox.setEditable(false);
         scoreBox.setOpaque(false);
 
-        timeBox.setBounds(360, TABLE_HEIGHT - 70, 120, 30);
+        timeBox.setBounds(TABLE_WIDTH - 120, gameBarYOff + incGameBarCompYOff(), 120, 30);
         timeBox.setText("Seconds: 0");
         timeBox.setEditable(false);
         timeBox.setOpaque(false);
     }
     private static void initializeMenuButtons(){
-        newGameButton.addActionListener(new NewGameListener());
+        addListenerIfNotPresent(newGameButton, new NewGameListener());
         newGameButton.setBounds(TABLE_WIDTH/2-70, TABLE_HEIGHT/2 -30, 120, 30);
 
-        showRulesButton.addActionListener(new ShowRulesListener());
+        addListenerIfNotPresent(showRulesButton, new ShowRulesListener());
         showRulesButton.setBounds(TABLE_WIDTH/2-70, TABLE_HEIGHT/2, 120, 30);
     }
+
     private static void startMainMenu(){
         table.removeAll();
         initializeMenuButtons();
         table.add(gameTitle);
         table.add(newGameButton);
         table.add(showRulesButton);
+        table.repaint();
     }
+
+    private static void initializeOptionsMenuButtonsButtons()
+    {
+        addListenerIfNotPresent(mainMenuButton, new MainMenuListener());
+        newGameButton.setBounds(TABLE_WIDTH/2-70, TABLE_HEIGHT/2 -30, 120, 30);
+    }
+
+    private static void startOptionsMenu()
+    {
+        table.removeAll();
+        initializeOptionsMenuButtonsButtons();
+        table.add(gameTitle);
+        table.add(mainMenuButton);
+        table.repaint();
+    }
+
     private static void dealNewGame() {
         for (int x = 0; x < 3; x++) {
             int hld = 0;
@@ -370,5 +419,13 @@ public class Solitaire {
             startMainMenu();
         }
 
+    }
+
+    private static class OptionsMenuListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            startOptionsMenu();
+        }
     }
 }

e6155bf8d2d31be0dbd7d8463466f48ed006a108
Benjamin-Bennett <33554511+Benjamin-Bennett@users.noreply.github.com>
2020-11-14 16:09:19 -0500

    Leaderboards
    Leaderboard implemented (shittily)
    Leaderboard data saved to persistent TXT file
    Leaderboard does NOT update from gameScore at this time, nor does it print to screen
    Leaderboard button added to main menu, prints to console currently

---
+++ CPSC-4900-Team-2-buggy/MySolitaire-master/Leaderboard.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+public class Leaderboard {
+    /**This class loads the leaderboard data, writes data to persistent storage
+     * and is responsible for painting the leaderboard data into
+     * a frame when the user selects the "Show leaderboard" button.
+     */
+    int gameScore = Solitaire.getScore();
+    private static String scoresTXT = "MySolitaire-master\\leaderboards.txt";
+    public static List<String> scores = new ArrayList<>();
+
+    /** No input method to read information from text file containing scores into an ArrayList
+     * @return ArrayList of scores found in file
+     *
+     */
+    public static List<String> initializeBoards(){
+        try {
+            BufferedReader read = new BufferedReader(new FileReader(scoresTXT));
+            String line = read.readLine();
+            if (line != null){
+                String[] temp = line.split(", ");
+                scores = new ArrayList<>(Arrays.asList(temp));
+            }
+            read.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return scores;
+    }
+
+    /**No return method to update ArrayList that contains leaderboard scores
+     *
+     * @param gameScore current value of score from running game
+     * @param scores ArrayList containing current values on leaderboard
+     */
+    private void addScore(int gameScore, List<String> scores){
+        boolean scoreSpot = false;
+        int x = 0;
+        while (scoreSpot == false && scores.size() > x ){
+            if (gameScore <= Integer.parseInt(scores.get(x))){
+                scoreSpot = true;
+            }
+            x++;
+        }
+        scores.add(x, String.valueOf(gameScore));
+    }
+
+    /**No return method to write current leaderboard held in ArrayList to persistent TXT file storage
+     *
+     * @param scores ArrayList containing current scores
+     */
+    private void updateFile(List<String> scores) {
+        try {
+            FileWriter write = new FileWriter(scoresTXT);
+            String updateScores = scores.toString();
+            updateScores = updateScores.replace("[", "");
+            updateScores = updateScores.replace("]", "");
+            write.write(updateScores);
+            write.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
